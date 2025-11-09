d4e5d959254013d33edaf1e595958940fcd6a43e
SamirShef <ssserin10@gmail.com>
2025-11-09 11:37:30 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # topazlang
 The Topaz programming language. Safety and speed!
+
+## Usage
+```bash
+topazc path/to/src.tp
+```
+Where `path/to/src` is relative path to source
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file main.cpp
+ *
+ * @brief Compiler entry point
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello from Topaz!\n";
+}
